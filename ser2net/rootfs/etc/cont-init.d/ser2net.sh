a9a36c7e1212ae2edc276ddf65e4f765d9c19e03
Clément Fouché <cfouche3005@gmail.com>
2025-09-14 00:18:24 +0200

    fix: update version to 1.0.7 in config.yaml fix: add mdns option to connection configuration in ser2net.sh

--- ser2net/rootfs/etc/cont-init.d/ser2net.sh
+++ ser2net/rootfs/etc/cont-init.d/ser2net.sh
@@ -43,6 +43,7 @@ for connection in $(bashio::config "connections|keys"); do
         echo "  accepter: tcp,${port}"
         echo "  connector: ${connector}"
         echo "  enable: on"
+        echo " mdns: false"
         echo "  options:"
         if bashio::config.true "connections[${connection}].kickolduser"; then
             echo "    kickolduser: true"
